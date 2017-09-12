b07d432307dfc01a9fd5e336b2c5382d6fc0b96f
Kelvin Smith <oneKelvinSmith@gmail.com>
2017-09-12 12:39:06 +0100

    Add distillery and update config

--- lib/klingons_web/endpoint.ex
+++ lib/klingons_web/endpoint.ex
@@ -49,7 +49,10 @@ defmodule KlingonsWeb.Endpoint do
   def init(_key, config) do
     if config[:load_from_system_env] do
       port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
-      {:ok, Keyword.put(config, :http, [:inet6, port: port])}
+      config
+      |> Keyword.put(:http, [:inet6, port: port])
+      |> Keyword.put(:url, [host: config[:url][:host], port: port])
+      {:ok, config}
     else
       {:ok, config}
     end
